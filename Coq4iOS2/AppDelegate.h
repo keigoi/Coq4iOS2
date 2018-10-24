e36a8f8b301456078a3ea5e4eb7bfa5fe7f14f10
Keigo Imai <keigo.imai@gmail.com>
2018-10-24 13:19:48 +0900

    Initial Commit

---
+++ Coq4iOS2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coq4iOS2
+//
+//  Created by keigoi on 2018/10/24.
+//  Copyright © 2018 keigoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coq4iOS2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Coq4iOS2
+//
+//  Created by keigoi on 2018/10/24.
+//  Copyright © 2018 keigoi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
