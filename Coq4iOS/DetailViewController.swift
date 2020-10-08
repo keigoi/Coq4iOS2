5708c8e898fe04f93dab2d03d5963490531c4820
Keigo Imai <keigo.imai@gmail.com>
2020-10-08 15:16:57 +0900

    overhaul basic behaviour

--- Coq4iOS/DetailViewController.swift
+++ Coq4iOS/DetailViewController.swift
@@ -27,8 +27,12 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
         startCoq()
+        // fputs(readStdout(), stderr);
         readStdout({(msg:String?) -> Void in
             fputs(msg, stderr)
+//            eval("Theorem id:forall a,a -> a.", {(res:Bool, msg:String?) -> Void in
+//                fputs(msg, stderr);
+//            })
         });
 //        let goalAreaController = childViewControllers[0] as! GoalAreaViewController
 //        eval("Theorem modus_ponens: forall (A B: Prop), (A -> B) -> A -> B.", {(res:Bool, ans:String?) -> Void in
@@ -63,27 +67,55 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var runButton: UIButton!
     @IBOutlet weak var runText: UITextField!
     
-    var textFieldString = ""
     
     @IBAction func runAction(_ sender: Any) {
-        // TextField から文字を取得
-        textFieldString = runText.text!
-        //NSLog(textFieldString)
-        scriptArea.insertText(textFieldString + "\n")
-        setViewController(str: textFieldString)
-        // TextField の中身をクリア
-        runText.text = ""
+        evalPhrase()
     }
     
-    func setViewController(str: String){
-        // 親から Container View への受け渡し
-        let goalAreaController = childViewControllers[0] as! GoalAreaViewController
-        eval(str, {(res:Bool, ans:String?) -> Void in
-            fputs(ans, stderr);
-            goalAreaController.textView.text = ans!
-        });
-
+    @IBAction func backAction(_ sender: Any) {
+        back();
+        self.refreshTextarea()
     }
+    
+    func evalPhrase(){
+        let whole = scriptArea.text!
+        let rest = String(whole[String.Index.init(encodedOffset: lastPos())...])
+        
+        nextPhraseRange(rest, {(range_:NSRange) -> Void in
+            if (range_.location < 0) {
+                return;
+            }
+            
+            let range = NSRange(location:0, length:range_.length + range_.location)
+            guard let realRange = Range(range, in:rest) else {
+                return
+            }
+            let str = String(rest[realRange])
+            
+            eval(str, {(success:Bool, ans:String?) -> Void in
+                fputs(ans, stderr);
+                let goalAreaController = self.childViewControllers[0] as! GoalAreaViewController
+                goalAreaController.textView.text = ans!
+                if (success) {
+                    self.refreshTextarea()
+                }
+            });
+        })
+    }
+    
+    func refreshTextarea() {
+        let str = NSMutableAttributedString(string: self.scriptArea.text)
+        
+        // coloring the evaluated range
+        let evaluatedRange = NSRange(location:0, length: lastPos())
+        str.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.blue, range: evaluatedRange)
+                
+        let font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
+        str.addAttribute(NSAttributedStringKey.font, value: font, range: NSRange(location:0, length:str.length))
+        self.scriptArea.attributedText = str
+        
+    }
+    
 
     
 }
